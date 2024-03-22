20409817beda18bf245b6f4f1d3937b23a5030c1
cfritz343 <82992621+cfritz343@users.noreply.github.com>
2024-03-22 17:04:26 -0500

    Add files via upload

---
+++ Assets/Assets/Derek/Scripts/DH_EnemyController.cs
@@ -0,0 +1,107 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class DH_EnemyController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public int direction;
+
+    public bool grounded;
+    public bool chasesEnemy;
+    public bool avoidsFalling;
+
+    public float fallSpeed = -1f;
+    public float walkingSpeed = 3f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        rb = GetComponent<Rigidbody2D>();
+        GetStartingDirection();
+        grounded = true;
+        rb.velocity = new Vector2(walkingSpeed * direction, 0);
+
+    }
+    
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if (Camera.main.WorldToViewportPoint(transform.position).x < 0.1 || Camera.main.WorldToViewportPoint(transform.position).x > 0.9)
+        {
+            ChangeDirection();
+            rb.velocity = new Vector2(walkingSpeed * direction, 0);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    { 
+        if (collision.collider.gameObject.tag == "Floor")
+        {
+            grounded = true;
+            fallSpeed = 0;
+        }
+
+        if (collision.collider.gameObject.tag == "Player")
+        {
+            CollisionAttack(collision.collider.gameObject);
+        }
+    }
+
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        
+    }
+
+
+    private void ChangeDirection()
+    {
+        direction *= -1;
+    }
+
+
+    private void CollisionAttack(GameObject player)
+    { 
+        player.GetComponent<NT_PlayerStats>().currentHp -= 5;
+        KnockbackObject(player);
+    }
+
+
+    //performs knockback effect on player when certain attacks are performed
+    private void KnockbackObject(GameObject player)
+    {
+        Rigidbody2D player_rb = player.GetComponent<Rigidbody2D>();
+
+        //how hard enemy will be knocked back
+        float kbForce = 4f;
+
+        //determine direction which to send enemy
+        Vector2 kbDirection = (player.transform.position - transform.position).normalized;
+
+        //kill player's momentum before applying force
+        player_rb.velocity = Vector2.zero;
+
+        //apply knockback to player
+        player_rb.AddForce(kbForce * kbDirection, ForceMode2D.Impulse);
+    }
+
+
+    private void GetStartingDirection()
+    {
+        int x = Random.Range(0, 50);
+
+        if (x % 2 == 0)
+            direction = 1;
+        else
+            direction = -1;
+    }
+
+
+}
---
+++ Assets/Assets/Nathan/Scripts/NT_HitboxController.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class NT_HitboxController : MonoBehaviour
+{
+    public float damageAmount = 10f;
+    public GameObject AttackDirection;
+    float lifetime = 0.5f; // Adjust the lifetime as needed
+    private float timer;
+    bool isActive;
+
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        if (isActive) { 
+            // Check if the hitbox should be destroyed based on the timer
+            if (Time.time - timer >= lifetime)
+            {
+                // Destroy the hitbox (or deactivate it, depending on your design)
+                AttackDirection.SetActive(false);
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            print("attack hit");
+            DH_EnemyHealth enemyHealth = other.GetComponent<DH_EnemyHealth>();
+            if (enemyHealth.currentHealth != 0)
+            {
+                print("damage");
+                enemyHealth.TakeDamage(damageAmount);
+            }
+
+            AttackDirection.SetActive(false);
+        }
+    }
+
+    private void OnEnable()
+    {
+        timer = Time.time;
+        isActive = true;
+    }
+
+    private void OnDisable()
+    {
+        isActive = false;
+    }
+}
