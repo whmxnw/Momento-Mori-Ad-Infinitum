a7b75b1151efdd2ce206e29a861292029b879391
nttucker28 <nttucker28@gmail.com>
2024-04-18 17:42:52 -0500

    deleted artifacts

--- Assets/Nathan/Scripts/NT_ItemDatabase.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "New Item Database", menuName = "Create Item Database")]
-public class NT_ItemDatabase : ScriptableObject
-{
-    public List<NT_Item.ItemData> items = new List<NT_Item.ItemData>(); 
-}
